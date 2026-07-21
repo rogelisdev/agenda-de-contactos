96255d1a7e5c3c3ac155943ff886e6b6df7baf6f
MariaGongora <margongora2004@gmail.com>
2026-07-21 16:05:45 -0500

    Se agrega tabla a la vista

--- src/main/java/org/example/view/AgendaView.java
+++ src/main/java/org/example/view/AgendaView.java
@@ -1,24 +1,31 @@
 package org.example.view;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 
 public class AgendaView {
     JFrame frame = new JFrame("Agenda de Contactos");
 
-    //Distintas vistas
-    JPanel panel = new JPanel();
-    JPanel  panel2 = new JPanel();
+    //Tabla
+    String[] columnas = {"Nombre", "Apellido", "Telefono", "Correo"};
+    DefaultTableModel model = new DefaultTableModel(columnas, 0);
+    JTable table = new JTable(model);
+
+    JScrollPane scroll = new JScrollPane(table);
 
     //Botones
     JButton buttonAgregar = new JButton("Agregar");
     JButton buttonActualizar = new JButton("Actualizar");
     JButton buttonEliminar = new JButton("Eliminar");
 
-    //Titulos
+    //Textos de la ventana
     JLabel labelNombre = new JLabel("Nombre: ");
     JLabel labelApellido = new JLabel("Apellido: ");
     JLabel labelTelefono = new JLabel("Telefono: ");
-    JLabel labelEmail = new JLabel("Correo");
+    JLabel labelEmail = new JLabel("Correo: ");
+    JLabel labelTitulo = new JLabel("REGISTRO");
+    JLabel labelContactos = new JLabel("CONTACTOS");
 
     //Campos de texto para el usuario
     JTextField nombre = new JTextField();
@@ -45,15 +52,50 @@ public class AgendaView {
            menuBar.add(menu); //Barra del menu
            frame.setJMenuBar(menuBar); //Agregar la barra del menu
 
-           nombre.setBounds(70, 100, 200, 30);
-           apellido.setBounds(70, 130, 200, 30);
-           telefono.setBounds(70, 160, 200, 30);
-           email.setBounds(70, 190, 200, 30);
+           //Texto
+           labelNombre.setBounds(35,90,70,30);
+           labelApellido.setBounds(35,130,70,30);
+           labelTelefono.setBounds(35,170,70,30);
+           labelEmail.setBounds(35,210,70,30);
+
+           labelTitulo.setBounds(100,20,200,40);
+           labelContactos.setBounds(580,20,200,40);
+
+
+           //Campos de texto
+           nombre.setBounds(100, 90, 200, 30);
+           apellido.setBounds(100, 130, 200, 30);
+           telefono.setBounds(100, 170, 200, 30);
+           email.setBounds(100, 210, 200, 30);
+
+           //Tabla contactos
+           scroll.setBounds(450, 75, 400, 200);
+
+           //Botones
+           buttonAgregar.setBounds(200,260,100,25);
+           buttonActualizar.setBounds(750,280,100,25);
+           buttonEliminar.setBounds(640,280,100,25);
+
+           //Fuentes
+           labelTitulo.setFont(new Font("Serif", Font.BOLD, 24));
+           labelContactos.setFont(new Font("Serif", Font.BOLD, 24));
 
            frame.add(nombre);
            frame.add(apellido);
            frame.add(telefono);
            frame.add(email);
+           frame.add(labelNombre);
+           frame.add(labelApellido);
+           frame.add(labelTelefono);
+           frame.add(labelEmail);
+           frame.add(labelTitulo);
+           frame.add(labelContactos);
+
+           frame.add(buttonAgregar);
+           frame.add(buttonActualizar);
+           frame.add(buttonEliminar);
+
+           frame.add(scroll);
 
            //Configuración del frame
            frame.setLayout(null);
