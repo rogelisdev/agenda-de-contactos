1a60b0e5a4b7baa2a427832ef8dcc592c4d66321
rogelisdev <rogelisdev@gmail.com>
2026-07-21 10:43:53 -0500

    Modelo contacto

--- src/main/java/org/example/model/Contacto.java
+++ src/main/java/org/example/model/Contacto.java
@@ -1,4 +1,55 @@
 package org.example.model;
 
 public class Contacto {
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+    public Contacto() {
+    }
+
+    public Contacto(String nombre, String apellido, String email, String telefono) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String toString() {
+        return nombre + " " + apellido + " - " + telefono + " - " + email;
+    }
 }
